d07828e18f93054c1127bb24614abc8283a509d4
AhmedElsayed101 <elsayedahmed945@gmail.com>
2020-04-10 10:54:30 +0200

    removing checking for request as a token must be passed

--- ayat/authorization.py
+++ ayat/authorization.py
@@ -12,20 +12,22 @@ def user_required(original_function):
     @wraps(original_function)
     def wrapper(*args, **kwargs):
         
-        if request.method=="GET":
+        # if request.method=="GET":
+        #     return original_function(*args, **kwargs)
+        # if request.method=="POST":
+
+        #we don't have to check for methode of the request as the token must be passed with the request
+
+        current_user_token = request.get_json('token')
+        current_user = User.query.filter_by(token = current_user_token).first()
+
+        if not current_user:
+            return jsonify({
+
+                'status': 404
+            })
+        else:
             return original_function(*args, **kwargs)
-        if request.method=="POST":
-
-            current_user_token = request.get_json('token')
-            current_user = User.query.filter_by(token = current_user_token).first()
-
-            if not current_user:
-                return jsonify({
-
-                    'status': 404
-                })
-            else:
-                return original_function(*args, **kwargs)
 
     return wrapper
 
@@ -35,27 +37,24 @@ def admin_required(original_function):
 
     @wraps(original_function)
     def wrapper(*args, **kwargs,):
-        
-        if request.method=="GET":
-            return original_function(*args, **kwargs)
-        if request.method=="POST":
-            current_user_token = request.get_json('token')
+     
+        current_user_token = request.get_json('token')
 
-            current_user = User.query.filter_by(token = current_user_token).first()
-            if not current_user:
-                return jsonify({
+        current_user = User.query.filter_by(token = current_user_token).first()
+        if not current_user:
+            return jsonify({
 
-                    'status': 404
-                })
+                'status': 404
+            })
 
-            current_user_admin_check = current_user.user_type_id
+        current_user_admin_check = current_user.user_type_id
 
-            if  current_user_admin_check != 1:
-                return jsonify({
+        if  current_user_admin_check != 1:
+            return jsonify({
 
-                    'status': 404
-                })
-        
+                'status': 404
+            })
+    
         return original_function(*args, **kwargs) 
     
        
