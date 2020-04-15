099d8639e16f4078ae455851ff8fa022ac35caf2
Mohamed El-lithy <mohamed.elliethy@hotmail.com>
2020-04-15 13:41:19 +0200

    Updated The users.py file

--- ayat/models/users.py
+++ ayat/models/users.py
@@ -13,7 +13,7 @@ class User(db.Model):
     name = db.Column(db.VARCHAR(40), nullable=False)
     public_id = db.Column(UUID(as_uuid=True),default=uuid.uuid4, unique=True,nullable=False)
     email = db.Column(db.VARCHAR(120), unique=True, nullable=False)
-    address_id = db.Column(db.Integer, db.ForeignKey("address.address_id"), nullable=False)
+    country_name = db.Column(db.VARCHAR(30), nullable=False)
     phone_number = db.Column(db.VARCHAR(15), unique=True, nullable=False)
     profile_picture = db.Column(db.VARCHAR(20)) ## FrontEnd will set a default Profile Picture
     birth_date = db.Column(db.Date, nullable=False)
@@ -30,8 +30,8 @@ class User(db.Model):
 
     
     def __repr__(self):
-    	Info_text = (f'User Id: {self.user_id}.\n'
-    		f'User Name: {self.name}.\n'
+    	Info_text = (f'{self.__class__.__name__} Id: {self.user_id}.\n'
+    		f'{self.__class__.__name__} Name: {self.name}.\n'
     		f'Public Id: {self.public_id}.\n'
     		f'E-mail: {self.email}.\n'
     		f'Address Id: {self.address_id}.\n'
@@ -47,89 +47,21 @@ class User(db.Model):
 
 #############################################################################################################
 
+Student_Guardian = db.Table('student_guardian', db.Model.metadata,
+    db.Column('user_guardian_id', db.Integer, primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.user_id'), nullable=False),
+    db.Column('guardian_id', db.Integer, db.ForeignKey('guardian.guardian_id'), nullable=False),  
+
+    UniqueConstraint('user_id','guardian_id'))
+
+
 class Student(User):
     __tablename__ = "student"
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
+    guardians = db.relationship('Guardian', secondary=Student_Guardian, backref='students')
 
     __mapper_args__ = {'polymorphic_identity':'student'}
 
-    def __repr__(self):
-    	Info_text = (f'User Id: {self.user_id}.') ## User is a Student
-
-    	return Info_text
-
-#############################################################################################################
-
-class Staff(User):
-    __tablename__ = "staff"
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True, unique=True)
-
-    __mapper_args__ = {'polymorphic_identity':'staff'}
-
-    def __repr__(self):
-    	Info_text = (f'User Id: {self.user_id}.')
-
-    	return Info_text
-
-#############################################################################################################
-
-class Address(db.Model):
-    __tablename__ = "address"
-    address_id = db.Column(db.Integer, primary_key=True)
-    country_name = db.Column(db.VARCHAR(30), unique=True, nullable=False)
-
-    def __repr__(self):
-    	Info_text = (f'Address Id: {self.address_id}.\n'
-    		f'Country Name: {self.country_name}.')
-
-    	return Info_text
-
-#############################################################################################################
-
-class Staff_Permission(db.Model):
-    __tablename__ = "staff_permission"
-    staff_permission_id = db.Column(db.Integer, primary_key=True)
-    permission_id = db.Column(db.SmallInteger, db.ForeignKey('permission.permission_id'), nullable=False )
-    staff_id = db.Column(db.Integer, db.ForeignKey('staff.user_id'), nullable=False )
-
-    def __repr__(self):
-    	Info_text = (f'Staff Permission Id: {self.staff_permission_id}.\n'
-    		f'Permission Id: {self.permission_id}.\n'
-    		f'Staff Id: {self.staff_id}.')
-
-    	return Info_text
-
-#############################################################################################################
-
-class Permission(db.Model):
-    __tablename__ = "permission"
-    permission_id = db.Column(db.Integer, primary_key=True)
-    permission_name = db.Column(db.VARCHAR , nullable=False)
-
-    def __repr__(self):
-    	Info_text = (f'Permission Id: {self.permission_id}.\n'
-    		f'Permission Name: {self.permission_name}.')
-
-    	return Info_text
-
-#############################################################################################################
-
-class Student_Guardian(db.Model):
-    __tablename__ = "student_guardian"
-    user_guardian_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
-    guardian_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
-
-    UniqueConstraint('user_id','guardian_id')
-
-    def __repr__(self):
-    	Info_text = (f'User Guardian Id: {self.user_guardian_id}.\n'
-    		f'User Id: {self.user_id}.\n'
-    		f'Guardian Id: {self.guardian_id}.')
-
-    	return Info_text
-
-#############################################################################################################
 
 class Guardian(db.Model):
     __tablename__ = "guardian"
@@ -138,8 +70,40 @@ class Guardian(db.Model):
     phone_number = db.Column(db.VARCHAR(15), unique=True, nullable=False)
 
     def __repr__(self):
-    	Info_text = (f'Guardian Id: {self.guardian_id}.\n'
-    		f'E-mail: {self.email}.\n'
-    		f'Phone Number: {self.phone_number}.')
+        Info_text = (f'Guardian Id: {self.guardian_id}.\n'
+            f'E-mail: {self.email}.\n'
+            f'Phone Number: {self.phone_number}.')
 
-    	return Info_text
+        return Info_text
+
+#############################################################################################################
+
+Staff_Permission = db.Table('staff_permission', db.Model.metadata,
+    db.Column('staff_permission_id', db.Integer, primary_key=True),
+    db.Column('permission_id', db.SmallInteger, db.ForeignKey('permission.permission_id'), nullable=False),
+    db.Column('staff_id', db.Integer, db.ForeignKey('staff.user_id'), nullable= False),  
+
+    UniqueConstraint('staff_id','permission_id'))
+
+
+class Staff(User):
+    __tablename__ = "staff"
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
+    permissions = db.relationship('Permission', secondary=Staff_Permission, backref='staff')
+
+    __mapper_args__ = {'polymorphic_identity':'staff'}
+
+
+class Permission(db.Model):
+    __tablename__ = "permission"
+    permission_id = db.Column(db.Integer, primary_key=True)
+    permission_name = db.Column(db.VARCHAR() , nullable=False)
+
+    def __repr__(self):
+        Info_text = (f'Permission Id: {self.permission_id}.\n'
+            f'Permission Name: {self.permission_name}.')
+
+        return Info_text
+
+#############################################################################################################
+
