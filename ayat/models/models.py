d74eae232ceb69097ba467635fc34101039784a0
Khaled Mohamed <khaledmzakria1@gmail.com>
2020-04-20 16:21:08 +0200

    ORM release v1 base

---
+++ ayat/models/models.py
@@ -0,0 +1,8 @@
+from ayat import app
+
+from ayat.models.users_model import *
+# from ayat.ORM_models.assessments_scheduling import *
+from ayat.models.programs import *
+from ayat.models.recitation import *
+
+
