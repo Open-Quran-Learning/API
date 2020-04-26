c632d01d010776ea2e599557634a2bfad78370f7
AhmedElsayed101 <elsayedahmed945@gmail.com>
2020-04-26 10:47:20 +0200

    fixing models importing

--- ayat/models/models.py
+++ ayat/models/models.py
@@ -3,6 +3,6 @@ from ayat import app
 from ayat.models.users import *
 # from ayat.ORM_models.assessments_scheduling import *
 from ayat.models.programs import *
-from ayat.models.recitation import *
+from ayat.models.recitations import *
 
 
